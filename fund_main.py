828d3eb8a085b38dafb1f5285e9269c4d78ebba2
frankwzhg <frankwzhg@hotmail.com>
2015-05-22 17:50:29 +0800

    new milestone for this project

--- fund_main.py
+++ fund_main.py
@@ -4,7 +4,8 @@ import os.path
 import op_database as op_db
 from time import strftime, gmtime, sleep, localtime
 import importlib
-import datetime
+
+
 # load data for each module
 sel_date = strftime("%Y-%m-%d", gmtime())
 
@@ -26,21 +27,23 @@ def run_module(module, table_name_list):
         print "moudule: " + module +" data frame: " + table_name + " is loaded sucessfully"
 
 if __name__ == "__main__":
-    day = datetime.datetime.now()+datetime.timedelta(days=1)
-    tomorrow =  day.strftime('%A')
-    close_date = ("Saturday", "Sunday")
+
+    close_date = ('Saturday', 'Sunday', '2015-06-22', '2015-10-01', '2015-10-02', '2015-10-05', '2015-10-06', '2015-10-07')
     current_date = strftime("%A", localtime())
-    if any(tomorrow in code for code in close_date):
-        print "abc"
+    today_date = strftime('%Y-%m-%d', gmtime())
+    if any(today_date in code for code in close_date):
+        quit()
     else:
-        print "def"
-    # run_module("stock_data", ['stock_data'])
-    # run_module("fund_stock_close_ETF_LOF_sina", ['fund_Close_SK', 'fund_ETF_SK', 'fund_LOF_SK'])
-    # run_module("fund_close_ETF_LOF_sina", ['fund_Close_FD', 'fund_ETF_FD', 'fund_LOF_FD'])
-    # run_module("fund_creative_sina", ["fund_creative_SK", "fund_creative_FD"])
-    # run_module("fund_top_stock", ['fund_Close_top_stocks', 'fund_ETF_top_stocks', 'fund_LOF_top_stocks', 'fund_creative_top_stocks'])
-    # run_module("fund_information", ['fund_Close_FD_info', 'fund_ETF_FD_info', 'fund_LOF_FD_info', 'fund_creative_FD_info'])
-    # run_module("fund_cal_rate", ['fund_Close_rate', 'fund_ETF_rate', 'fund_LOF_rate', 'fund_creative_rate'])
+        if any(current_date in code for code in close_date):
+            quit()
+        else:
+            run_module("stock_data", ['stock_data'])
+            run_module("fund_stock_close_ETF_LOF_sina", ['fund_Close_SK', 'fund_ETF_SK', 'fund_LOF_SK'])
+            run_module("fund_close_ETF_LOF_sina", ['fund_Close_FD', 'fund_ETF_FD', 'fund_LOF_FD'])
+            run_module("fund_creative_sina", ["fund_creative_SK", "fund_creative_FD"])
+            run_module("fund_top_stock", ['fund_Close_top_stocks', 'fund_ETF_top_stocks', 'fund_LOF_top_stocks', 'fund_creative_top_stocks'])
+            run_module("fund_information", ['fund_Close_FD_info', 'fund_ETF_FD_info', 'fund_LOF_FD_info', 'fund_creative_FD_info'])
+            run_module("fund_cal_rate", ['fund_Close_rate', 'fund_ETF_rate', 'fund_LOF_rate', 'fund_creative_rate'])
 
 
 # if __name__ == "__main__":
